1e2afab2c3af976b2146eab36b1392bac4d5a728
Esteban Monguí <esteband.itorres@gmail.com>
2020-06-28 20:36:18 -0500

    challenge 03 solved

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 # Resolve the problem!!
 
+import re
 
 def run():
-    # Start coding here
-
+    # Start coding here  
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
+        pattern = re.compile(r'[a-z]')
+        for line in f:
+            match = pattern.findall(line)
+        print (''.join(match))
 
 if __name__ == '__main__':
     run()
+
+
